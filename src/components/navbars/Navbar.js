f84c5d56796ab980b495904e45dc5ba4da62420c
fabianchua6 <fabianchua6@gmail.com>
2022-07-26 22:38:41 +0800

    refactoring and cleanup

--- src/components/navbars/Navbar.js
+++ src/components/navbars/Navbar.js
@@ -5,8 +5,8 @@ export default function Navbar() {
   return (
     <nav class="bg-transparent border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
       <div class="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="https://flowbite.com/" class="flex items-center">
-          <img src={logo} class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+        <a href="#" class="flex items-center">
+          <img src={logo} class="mr-3 h-6 sm:h-9" alt="Hackrift Logo" />
           <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
             {/* HackRift */}
           </span>
