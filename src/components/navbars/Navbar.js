e3c99a61d1a5a12ceb106a43440a7ec969ca2969
fabianchua6 <fabianchua6@gmail.com>
2022-07-27 00:57:28 +0800

    refactor navbar

--- src/components/navbars/Navbar.js
+++ src/components/navbars/Navbar.js
@@ -3,103 +3,55 @@ import logo from "../../assets/logo.png";
 
 export default function Navbar() {
   return (
-    <nav class="bg-transparent border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
-      <div class="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="#" class="flex items-center">
-          <img src={logo} class="mr-3 h-6 sm:h-9" alt="Hackrift Logo" />
-          <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-            {/* HackRift */}
-          </span>
-        </a>
-        <div class="flex md:order-2">
-          <button
-            type="button"
-            class="text-white bg-darkPurple hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Join the Hackathon
-          </button>
-          <button
-            data-collapse-toggle="mobile-menu-4"
-            type="button"
-            class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="mobile-menu-4"
-            aria-expanded="false"
-          >
-            <span class="sr-only">Open main menu</span>
-            <svg
-              class="w-6 h-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
+      <nav className="navbar bg-transparent py-4 ">
+        <div className="navbar-start justify-between grow md:grow-none lg:justify-start">
+          <div className="dropdown">
+            <label tabIndex="0" className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+            </label>
+            <ul
+              tabIndex="0"
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <path
-                fill-rule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-            <svg
-              class="hidden w-6 h-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </button>
+              <li>
+                <a>Item 1</a>
+              </li>
+              <li>
+                <a>Item 2</a>
+              </li>
+              <li>
+                <a>Item 3</a>
+              </li>
+            </ul>
+          </div>
+          <a className="btn-link btn">
+            <img src={logo} className="mr-3 h-6 sm:h-9" alt="Hackrift Logo" />
+          </a>
         </div>
-        <div
-          class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1"
-          id="mobile-menu-4"
-        >
-          <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+        <div className="navbar-center hidden lg:flex" >
+          <ul className="menu menu-horizontal  p-0">
             <li>
-              <AnchorLink
-                href="#about"
-                className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-              >
-                About
-              </AnchorLink>
+              <a href="#about" className="btn btn-ghost mr-2">About</a>
             </li>
             <li>
-              <AnchorLink
-                href="#schedule"
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Schedule
-              </AnchorLink>
+              <a href="#schedule" className="btn btn-ghost mr-2">Schedule</a>
             </li>
             <li>
-              <AnchorLink
-                href="#prizes"
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Prizes
-              </AnchorLink>
+              <a href="#prizes" className="btn btn-ghost mr-2">Prizes</a>
             </li>
             <li>
-              <AnchorLink
-                href="#sponsors"
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Sponsors
-              </AnchorLink>
+              <a href="#sponsors" className="btn btn-ghost mr-2">Sponsors</a>
             </li>
             <li>
-              <AnchorLink
-                href="#contact-us"
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Contact Us
-              </AnchorLink>
+              <a href="#contact-us" className="btn btn-ghost">Contact Us</a>
             </li>
           </ul>
         </div>
-      </div>
-    </nav>
+        <div className="navbar-end hidden lg:flex">
+          <a className="btn btn-primary">
+            Join the Hackathon
+          </a>
+        </div>
+      </nav>
   );
 }
