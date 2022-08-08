f9d877bba29a4920caa105ceb1180b55a78868fb
Chen JiaJun <cjj8899@gmail.com>
2022-08-08 19:23:02 +0800

    Refactor to remove warnings

--- src/containers/schedule/Schedule.js
+++ src/containers/schedule/Schedule.js
@@ -21,27 +21,19 @@ export default function Schedule() {
 							<td>8:30pm</td>
 							<td>Consultation Session via Zoom #1</td>
 						</tr>
-					</tbody>
-					<thead>
 						<tr>
 							<th colSpan="3">
 								Day 2: Saturday, 15 October 2022
 							</th>
 						</tr>
-					</thead>
-					<tbody>
 						<tr className="border-y-2 border-primary">
 							<td>2:00pm</td>
 							<td>4:00pm</td>
 							<td>Consultation Session via Zoom #2</td>
 						</tr>
-					</tbody>
-					<thead>
 						<tr>
 							<th colSpan="3">Day 3: Sunday, 16 October 2022</th>
 						</tr>
-					</thead>
-					<tbody>
 						<tr className="border-y-2 border-primary">
 							<td>2:30pm</td>
 							<td>~6:30pm</td>
