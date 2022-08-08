9b15f0c8f201f46b46f7e8a1c47d804f42934cc4
Chen JiaJun <cjj8899@gmail.com>
2022-08-08 23:57:26 +0800

    Updated Sponsor
    Added GovTech logo and SIT Logo

--- src/containers/sponsors/Sponsors.js
+++ src/containers/sponsors/Sponsors.js
@@ -1,9 +1,24 @@
 import Content from "../commons/Content";
+import govTechLogo from "../../assets/govTechLogo.png";
+import SITLogo from "../../assets/SITLogo.png";
 
 export default function Sponsors() {
-  return (
-    <div id="sponsors">
-      <Content title="Sponsors">GovTech Logo and maybe writeup here</Content>
-    </div>
-  );
+	return (
+		<div id="sponsors">
+			<Content title="Sponsors">
+				<p>GovTech Logo and maybe writeup here</p>
+				<div className="flex flex-wrap justify-center items-center">
+					<a
+						href="https://www.tech.gov.sg/careers/students-and-graduates/"
+						target="_blank"
+					>
+						<img src={govTechLogo} className="my-5 mx-5"></img>
+					</a>
+					<a href="https://www.singaporetech.edu.sg/" target="_blank">
+						<img src={SITLogo} className="my-5 mx-5"></img>
+					</a>
+				</div>
+			</Content>
+		</div>
+	);
 }
