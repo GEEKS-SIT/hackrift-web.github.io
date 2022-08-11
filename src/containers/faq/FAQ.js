e461f120005e98aaecfa8819505ad20613bc75ba
Fabian <fabianchua6@gmail.com>
2022-08-12 00:17:01 +0800

    update faq

--- src/containers/faq/FAQ.js
+++ src/containers/faq/FAQ.js
@@ -45,25 +45,29 @@ export default function FAQ() {
     <div id="FAQ">
       <Content title="FAQ">
         <div class="space-y-4">
-          <Item title="The quick, brown fox jumps over a lazy dog?">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque rem aperiam, eaque ipsa quae.
+          <Item title="When is HackRifT?">
+            HackRift is a weekend hackathon that will run from 14th October 2022
+            to 16th October 2022.
           </Item>
-          <Item title="The quick, brown fox jumps over a lazy dog?">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque rem aperiam, eaque ipsa quae.
+          <Item title="Do we have to pay for this hackathon?">
+            Admission is free and includes mentors, swag, resources, opportunity
+            to make new friends and a memorable experience!
           </Item>
-          <Item title="The quick, brown fox jumps over a lazy dog?">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque rem aperiam, eaque ipsa quae.
+          <Item title="What if I don’t have a team?">
+            Don't worry! You may still sign up individually and we will helping
+            you find people to work with.
           </Item>
-          <Item title="The quick, brown fox jumps over a lazy dog?">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque rem aperiam, eaque ipsa quae.
+          <Item title="How do teams work?">
+            Your team must minimally have 3 members and can have up to 5 people!
+            You may sign up together with your friends and hack away!
           </Item>
-          <Item title="The quick, brown fox jumps over a lazy dog?">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque rem aperiam, eaque ipsa quae.
+          <Item title="When is the application opening?">
+            Registration opens on 12th August 2022! Click{" "}
+            <a href="https://forms.gle/gXjE8Q3a35VwVpjY7">here</a> to apply now!
+          </Item>
+          <Item title="I still have questions!">
+            Please email us at sit.ict.smc@outlook.com or geeks.sit@gmail.com if
+            you have any questions or concerns : )
           </Item>
         </div>
       </Content>
