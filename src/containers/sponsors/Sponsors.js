a02ec36f29de9d83c19057e8564e0f002b846c3d
Fabian <fabianchua6@gmail.com>
2022-08-16 19:58:23 +0800

    update sponsors logo and style

--- src/containers/sponsors/Sponsors.js
+++ src/containers/sponsors/Sponsors.js
@@ -6,21 +6,20 @@ export default function Sponsors() {
   return (
     <div id="sponsors">
       <Content title="Sponsors">
-        {/* pl cause sit logo too big. */}
-        <div className="flex flex-col md:flex-row justify-around items-center md:pl-32">
-          <a
-            href="https://www.tech.gov.sg/careers/students-and-graduates/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={govTechLogo} alt="" className="h-40 lg:h-64 my-4 md:mb-0"></img>
-          </a>
+        <div className="flex flex-col md:flex-row justify-around items-center ">
           <a
             href="https://www.singaporetech.edu.sg/"
             target="_blank"
             rel="noreferrer"
           >
-            <img src={SITLogo} alt="" className="h-40 lg:h-52 my-4 md:mb-0"></img>
+            <img src={SITLogo} alt="" className="h-40 my-4 md:mb-0"></img>
+          </a>
+          <a
+            href="https://www.tech.gov.sg/careers/students-and-graduates/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={govTechLogo} alt="" className="h-48 my-4 md:mb-0"></img>
           </a>
         </div>
       </Content>
